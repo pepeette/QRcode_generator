aaf41b6eee9b5f55f8f1d95943df91909bc5db57
pepeette <59664130+pepeette@users.noreply.github.com>
2023-02-09 15:40:51 +0700

    create app

---
+++ QRcode.py
@@ -0,0 +1,47 @@
+#!pip install pillow
+#!pip install qrcode
+#!pip install "qrcode[pil]"
+#!pip install qrcode-xcolor
+#!pip install git+https://github.com/lincolnloop/python-qrcode.git
+
+import qrcode
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.moduledrawers import CircleModuleDrawer
+from  qrcode.image.styles.colormasks import SolidFillColorMask
+from PIL import Image, ImageDraw
+
+# taking url or text
+url = 'https://www.linkedin.com/in/laetitiahoquetis'
+
+#instantiate library
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,
+    border=4)
+qr.add_data(url)
+
+#define the corners, called eyes
+qr_eyes_img = qr.make_image(image_factory=StyledPilImage,
+                            eye_drawer=RoundedModuleDrawer(radius_ratio=1.2),
+                            color_mask=SolidFillColorMask(back_color=(255, 255, 255), front_color=(242, 106, 73)))
+
+#define logo inside
+qr_img = qr.make_image(image_factory=StyledPilImage,
+                       module_drawer=CircleModuleDrawer(),
+                       color_mask=SolidFillColorMask(front_color=(2, 175, 185)),
+                       embeded_image_path="logo.png")
+
+#define eyes
+def style_eyes(img):
+    img_size = img.size[0]
+    eye_size = 70 #default
+    quiet_zone = 40 #default
+    mask = Image.new('L', img.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.rectangle((40, 40, 110, 110), fill=255)
+    draw.rectangle((img_size-110, 40, img_size-40, 110), fill=255)
+    draw.rectangle((40, img_size-110, 110, img_size-40), fill=255)
+    draw.rectangle((img_size-110, img_size-110, img_size-40, img_size-40), fill=255)
+    return mask
+
+mask = style_eyes(qr_img)
+final_img = Image.composite(qr_eyes_img, qr_img, mask)
+final_img.save('QR.png')
